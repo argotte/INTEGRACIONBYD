f8a11a2c5488194d5d7301c52b245d2b76a4fccb
diegoargottez@gmail.com <diegoargottez@gmail.com>
2022-09-20 16:23:07 -0400

    hola humberto

---
+++ INTEGRACIONBYD/Form1.cs
@@ -0,0 +1,29 @@
+﻿using Servicio;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace INTEGRACIONBYD
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string answer;
+            Clientes client = new Clientes();
+            answer = client.GetClient("S100800");
+            MessageBox.Show(answer);
+        }
+    }
+}
---
+++ Servicio/Clientes.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Servicio.ConsultarCliente;
+
+namespace Servicio
+{
+    public class Clientes
+    {
+        //string _id;
+        QuerySupplierInClient cliente = new QuerySupplierInClient();
+  
+
+        public Clientes()
+        {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            cliente.ClientCredentials.UserName.UserName = "_OrionDiego";
+            cliente.ClientCredentials.UserName.Password = "Diego12345";
+        }
+        public string  GetClient(string id) 
+        {
+            SupplierByElementsQueryMessage_sync request = new SupplierByElementsQueryMessage_sync();
+            string rspst="";
+            try
+            {
+
+                request.SupplierSelectionByElements = new SupplierByElementsQuerySelectionByElements ();
+
+                request.SupplierSelectionByElements.SelectionByInternalID = new SelectionByIdentifier[0];
+                request.SupplierSelectionByElements.SelectionByInternalID[0] = new SelectionByIdentifier();
+                request.SupplierSelectionByElements.SelectionByInternalID[0].InclusionExclusionCode = "I";
+                request.SupplierSelectionByElements.SelectionByInternalID[0].IntervalBoundaryTypeCode = "1";
+                request.SupplierSelectionByElements.SelectionByInternalID[0].LowerBoundaryIdentifier = "S100800";
+
+
+
+                var response = cliente.FindByElements(request);
+
+                rspst = response.Supplier[0].FirstLineName;
+                return rspst;
+            }
+            catch (Exception ex )
+            {
+
+                return rspst = ex.Message;
+            }
+
+
+
+
+
+
+
+
+            
+
+        }
+
+    }
+}
